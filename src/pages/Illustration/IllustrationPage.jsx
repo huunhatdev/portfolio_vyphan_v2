ff9ea50e50e3112ab3d1c878ffccb4b6315326fa
huunhatdev <nhatnh98@gmail.com>
2024-09-07 09:42:13 +0700

    fix

--- src/pages/Illustration/IllustrationPage.jsx
+++ src/pages/Illustration/IllustrationPage.jsx
@@ -6,7 +6,7 @@ import IllustratedEducation from './components/IllustratedEducation';
 const IllustrationPage = () => {
     return (
         <Fragment>
-            <TitleSection color="#FDDBDE" title={`View my first illustrated\nchildren's education book!`} />
+            <TitleSection color="#FDDBDE" title={`View my first illustrated children's education book!`} />
             <IllustratedEducation />
             <TitleSection color="#DBEBBB" title={`My other illustrated projects`} />
             <IllustratedProject />
