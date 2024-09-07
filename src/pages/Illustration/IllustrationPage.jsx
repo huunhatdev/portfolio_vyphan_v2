6e5c3152ed889213f2a5bf6d5da3b09841caa4e3
huunhatdev <nhatnh98@gmail.com>
2024-09-07 09:54:52 +0700

    hidden education

--- src/pages/Illustration/IllustrationPage.jsx
+++ src/pages/Illustration/IllustrationPage.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from 'react';
 import TitleSection from '../../components/TitleSection';
 import IllustratedProject from './components/illustratedProject';
-import IllustratedEducation from './components/IllustratedEducation';
+// import IllustratedEducation from './components/IllustratedEducation';
 
 const IllustrationPage = () => {
     return (
         <Fragment>
             <TitleSection color="#FDDBDE" title={`View my first illustrated children's education book!`} />
-            <IllustratedEducation />
+            {/* <IllustratedEducation /> */}
             <TitleSection color="#DBEBBB" title={`My other illustrated projects`} />
             <IllustratedProject />
         </Fragment>
--- src/pages/Illustration/components/IllustratedProject.jsx
+++ src/pages/Illustration/components/IllustratedProject.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { getImage } from '../../../utils/image';
+import { getImage } from 'utils/image';
 
 const IllustratedProject = () => {
     const listImage = [
