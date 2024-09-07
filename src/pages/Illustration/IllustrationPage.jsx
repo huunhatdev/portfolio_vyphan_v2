675b0a94ad55690f85d10bd9ae5eee7a445ecb0b
huunhatdev <nhatnh98@gmail.com>
2024-09-07 09:57:59 +0700

    hidden IllustratedProject

--- src/pages/Illustration/IllustrationPage.jsx
+++ src/pages/Illustration/IllustrationPage.jsx
@@ -1,15 +1,15 @@
 import { Fragment } from 'react';
 import TitleSection from '../../components/TitleSection';
-import IllustratedProject from './components/illustratedProject';
-// import IllustratedEducation from './components/IllustratedEducation';
+// import IllustratedProject from './components/illustratedProject';
+import IllustratedEducation from './components/IllustratedEducation';
 
 const IllustrationPage = () => {
     return (
         <Fragment>
-            <TitleSection color="#FDDBDE" title={`View my first illustrated children's education book!`} />
-            {/* <IllustratedEducation /> */}
+            <TitleSection color="#FDDBDE" title={`View my first illustrated\nchildren's education book!`} />
+            <IllustratedEducation />
             <TitleSection color="#DBEBBB" title={`My other illustrated projects`} />
-            <IllustratedProject />
+            {/* <IllustratedProject /> */}
         </Fragment>
     );
 };
