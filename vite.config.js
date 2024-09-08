b4d67d18e8dddfa19becc93f0d5a9844736128e2
huunhatdev <nhatnh98@gmail.com>
2024-09-08 13:04:53 +0700

    setup build file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [react()],
     resolve: {
         alias: {
@@ -10,5 +11,15 @@ export default defineConfig({
             hooks: '/src/hooks',
             utils: '/src/utils'
         }
+    },
+    preview: {
+        port: 8080,
+        strictPort: true
+    },
+    server: {
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: 'http://0.0.0.0:8080'
     }
 });
